36208620b020b600336526a39c6f4f2f8f22ad10
Thuria234naing <113816550+Thuria234naing@users.noreply.github.com>
2025-04-28 21:47:59 +0630

    layout souk ny dr (for nav and footer) a khon

---
+++ src/data/NavData.js
@@ -0,0 +1,15 @@
+// src/data/NavData.js
+export const NavData = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Majors",
+      path: "/majors",
+    },
+    {
+      name: "About",
+      path: "/about",
+    },
+  ];
